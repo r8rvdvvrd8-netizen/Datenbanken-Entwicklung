3274fd821e54c9b5fab45d91eddc566e9fa1b94d
Niklas-Steigmiller <steigmiller.niklas@gmx.de>
2026-04-15 10:34:19 +0200

    deleted:    Verbindung 	new file:   Verbindung.php 	new file:   index.php

---
+++ Verbindung.php
@@ -0,0 +1,9 @@
+<?php
+$host = "localhost";
+$user = "gruppe19";
+$password = "{yI)X2)vN7w1";
+$db = "gruppe19";
+
+$connection = mysqli_connect($host, $user, $password);
+mysqli_select_db($connection, $db);
+?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Datenbanken-Entwicklung</title>
+        </head>
+    <body>
+        <h1>Datenbanken-Entwicklung</h1>
+        </body>
+</html>
