4081fe4321cafd8a28fdd858a82816fd0b369a9a
Niklas Steigmiller <r8rvdvvrd8@privaterelay.appleid.com>
2026-04-20 09:24:48 +0000

    Login v1

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 <html>
     <head>
         <title>Datenbanken-Entwicklung</title>
-        </head>
+    </head>
     <body>
-        <h1>Datenbanken-Entwicklung Anton, Christina,Niklas , hallo wie gehts test
-        
-        </h1>
-        </body>
+        <div style="text-align: right;">
+            <a href="pages/login.php"><button>Anmelden</button></a>
+        </div>
+        <h1>Datenbanken-Entwicklung Anton, Christina, Niklas, hallo wie gehts test</h1>
+    </body>
 </html>
---
+++ pages/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Anmelden</title>
+    </head>
+    <body>
+        <h1>Anmelden</h1>
+        <form method="post" action="">
+            <label>Benutzername:</label><br>
+            <input type="text" name="username"><br><br>
+            <label>Passwort:</label><br>
+            <input type="password" name="password"><br><br>
+            <button type="submit">Anmelden</button>
+        </form>
+        <br>
+        <a href="../index.php">Zurück zur Startseite</a>
+    </body>
+</html>
