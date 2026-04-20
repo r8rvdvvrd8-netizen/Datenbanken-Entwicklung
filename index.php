9129eab33fbdb4b5e1d5fe2651188adfccdcc6a1
Niklas Steigmiller <r8rvdvvrd8@privaterelay.appleid.com>
2026-04-20 10:08:12 +0000

    neu

--- index.php
+++ index.php
@@ -4,26 +4,10 @@
         <title>Anmelden</title>
     </head>
     <body>
-        <a href="../index.php">Zurück zur Startseite</a>
+        
         <h1>Anmelden</h1>
 
-        <?php
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            require_once '../Verbindung.php';
-
-            $loginname = mysqli_real_escape_string($connection, $_POST['loginname']);
-            $kennwort  = $_POST['kennwort'];
-
-            $result = mysqli_query($connection, "SELECT * FROM teamchef WHERE loginname = '$loginname'");
-            $user   = mysqli_fetch_assoc($result);
-
-            if ($user && password_verify($kennwort, $user['kennwort'])) {
-                echo "<p>Anmeldung erfolgreich. Willkommen, " . htmlspecialchars($user['vorname']) . "!</p>";
-            } else {
-                echo "<p style='color:red;'>Loginname oder Kennwort falsch.</p>";
-            }
-        }
-        ?>
+        
 
         <form method="post" action="">
             <label>Loginname:</label><br>
