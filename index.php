4cf79523081657e27e01fda33787b8fac2d029a8
Anton Nguyen <anton.nguyen@sap.com>
2026-04-20 11:11:39 +0200

    hallo wie gehts hinzugefügt

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
         <title>Datenbanken-Entwicklung</title>
         </head>
     <body>
-        <h1>Datenbanken-Entwicklung Anton, Christina,Niklas </h1>
+        <h1>Datenbanken-Entwicklung Anton, Christina,Niklas , hallo wie gehts
+        
+        </h1>
         </body>
 </html>
