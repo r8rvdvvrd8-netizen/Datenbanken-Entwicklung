e72a546919dc7b72a33765d6f9d1430afc13bee5
Anton Nguyen <anton.nguyen@sap.com>
2026-04-20 11:04:47 +0200

    anton, christina, niklas hinzugefügt

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
         <title>Datenbanken-Entwicklung</title>
         </head>
     <body>
-        <h1>Datenbanken-Entwicklung test</h1>
+        <h1>Datenbanken-Entwicklung Anton, Christina,Niklas </h1>
         </body>
 </html>
